63a02f368c6bda9615e78f684d61db8babf3191b
Renato Almeida <realmeidabr@gmail.com>
2022-07-21 00:11:03 -0300

    fix: add service layer to backend module (#17)
    * feat!: add backend module
    
    * fix: add services layer to backend module
    
    Co-authored-by: realmeidabr <realmeida.br@gmail.com>

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -4,22 +4,15 @@ class OrderController {
 
   getFeedPing = async (req, res) => {
     //console.log(req.body)
-    const { domain, orderId, state, lastChange, origin } = req.body
-    console.log(req.body)
+    const feedData = req.body
+    console.log(feedData)
+    const orderId = feedData.OrderId
+    console.log(orderId)
+    const orderData = await IOClient.getOrderData(orderId)
+    //console.log(orderData)
     res.send({ success: true })
   }
 }
 
 export default new OrderController
 
-/* TEST_CASE_1
-let userId = '35a37b17-a514-48b1-9d08-d4d6f83f0dc8'
-let balance = new BalanceController()
-balance.getBalance()
-*/
-
-// (req, res) => {
-//   const { orders } = req.body
-//   const newOrders = feedOrders(orders)
-//   res.send({ newOrders })
-// }
